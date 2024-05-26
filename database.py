8c237deb9cd02dabb9bb2630e70fe86df6b8725d
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-26 22:19:07 +0530

    New :: environment file

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = f"mysql://root@localhost:{3306}/geospatial_application?charset=utf8mb4&user=root&password=Kalindri%40123"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
