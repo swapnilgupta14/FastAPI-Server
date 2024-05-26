6e102503b543ada513d3f695f8f8d1a8dcc56bbd
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-26 18:51:41 +0530

    New :: endpoints definition

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
+    
+class UserBase(BaseModel):
+    username: str
+    email: str
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# end point to get user by id
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def read_user(user_id : int, db: db_dependency):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserBase, db: db_dependency):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    
